1880542c0f3b68abbfaf05e35ba10aecb3c50ce4
unknown <mohanvamshi96@gmail.com>
2022-12-09 23:45:01 +0530

    second commit

---
+++ src/api.js
@@ -0,0 +1,10 @@
+
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'e29d3951damsh658404427f37cf6p1a0424jsn902e82dec1f7',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL='https://wft-geo-db.p.rapidapi.com/v1/geo';
---
+++ src/components/current-weather/current-weather.js
@@ -0,0 +1,11 @@
+import "./current-weather.css"
+
+const CurrentWeather = () => {
+    return {
+
+        
+
+    };
+}
+
+export default CurrentWeather;
