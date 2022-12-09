876cee0cfcbd3bf7b012149b929588c3c6394c82
unknown <mohanvamshi96@gmail.com>
2022-12-10 02:40:19 +0530

    final commit

--- src/api.js
+++ src/api.js
@@ -8,3 +8,6 @@ export const geoApiOptions = {
 };
 
 export const GEO_API_URL='https://wft-geo-db.p.rapidapi.com/v1/geo';
+export const WEATHER_API_URL='https://api.openweathermap.org/data/2.5';
+
+export const WEATHER_API_KEY='cfe335f95bfc42afae7aefc2f95edc6a';
